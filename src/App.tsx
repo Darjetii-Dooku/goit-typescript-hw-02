64dcd2566508bfa1c99cc4f8568861c2818d9852
Darjetii-Dooku <7220339@gmail.com>
2024-05-12 23:16:50 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,65 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { SearchBar } from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import { requestImages, requestImagesByQuery } from "./services/api";
+import { requestImagesByQuery } from "./services/api";
 import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
 import ImageModal from "./components/ImageModal/ImageModal";
 
-function App() {
-  const [images, setImages] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [Iserror, setError] = useState(false);
-  const [query, setQuerry] = useState("");
-  const [page, setPage] = useState(1);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [modalImg, setModalImg] = useState(null);
-
-  const onSetQuery = (query) => {
-    setQuerry(query);
+interface ImageData {
+  id: string;
+  urls: {
+    small: string;
+    regular: string;
   };
+  alt_description: string;
+}
+
+function App() {
+  const [images, setImages] = useState<ImageData[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [query, setQuery] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [modalImg, setModalImg] = useState<ImageData | null>(null);
+
+  const onSetQuery = (query: string) => {
+    setQuery(query);
+  };
+
   useEffect(() => {
     if (query.length === 0) return;
-    console.log("hi");
-    async function fetchImages() {
+
+    const fetchImages = async () => {
+      setIsLoading(true);
       try {
-        setIsLoading(true);
         const data = await requestImagesByQuery(query, page);
         if (page === 1) {
           setImages(data.results);
         } else {
-          setImages((prevImages) => [...prevImages, ...data.results]);
+          setImages((prevImages) => [...(prevImages || []), ...data.results]);
         }
-        // setImages(data.results)
       } catch (error) {
-        setError(true);
+        setIsError(true);
       } finally {
         setIsLoading(false);
       }
-    }
+    };
+
     fetchImages();
   }, [query, page]);
-  const openModal = (img) => {
-    console.dir(img);
+
+  const openModal = (img: ImageData) => {
     setModalImg(img);
     setModalIsOpen(true);
-  }
-  // console.log("images: ", images);
+  };
 
   return (
     <>
       <SearchBar onSetQuery={onSetQuery} />
-      {Iserror && <ErrorMessage />}
+      {isError && <ErrorMessage />}
       {images && <ImageGallery images={images} isLoading={isLoading} openModal={openModal} />}
       {images && <LoadMoreBtn setPage={setPage} />}
       <ImageModal modalImg={modalImg} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />
--- src/services/api.types.ts
+++ src/services/api.types.ts
@@ -2,6 +2,7 @@ export interface ImageData {
     id: string;
     urls: {
       small: string;
+      regular: string;
     };
     alt_description: string;
   }
