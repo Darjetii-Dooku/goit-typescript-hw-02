4bc04d3e9f10b114256e824320168af2d5ab25ff
Darjetii-Dooku <7220339@gmail.com>
2024-05-13 11:16:59 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,13 +6,13 @@ const instance = axios.create({
 });
 
 export const requestImages = async (): Promise<ImagePromise> => {
-  const response: AxiosResponse<ImageData[]> = await instance.get(
+  const response: AxiosResponse<ImagePromise> = await instance.get(
     "/photos/?client_id=FVHLc2QFbVVLkf9JsQjfHpsnQcDmxTgEvRJr1m7vJBk"
   );
   return response.data;
 };
 export const requestImagesByQuery = async (query: string = "", page: number = 1): Promise<ImagePromise> => {
-  const response: AxiosResponse<ImageData[]> = await instance.get(
+  const response: AxiosResponse<ImagePromise> = await instance.get(
     `/search/photos/?client_id=FVHLc2QFbVVLkf9JsQjfHpsnQcDmxTgEvRJr1m7vJBk&query=${query}&page=${page}`
   );
   return response.data;
